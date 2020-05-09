8e15a86061cfdb6783789df712740116a89700f3
Carlos Henrique <ch.ferreir@hotmail.com>
2020-05-08 22:22:12 -0300

    Projeto criado

---
+++ src/main/java/me/henriquesouza/mcuml/McUmlApplication.java
@@ -0,0 +1,13 @@
+package me.henriquesouza.mcuml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McUmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/henriquesouza/mcuml/McUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.henriquesouza.mcuml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
