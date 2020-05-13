c36d931d4c28c9c1764aeeb6741200f81da7bbce
Carlos Henrique <ch.ferreir@hotmail.com>
2020-05-12 23:50:33 -0300

    Criando operação de instanciação

--- src/main/java/me/henriquesouza/mcuml/McUmlApplication.java
+++ src/main/java/me/henriquesouza/mcuml/McUmlApplication.java
@@ -1,13 +1,32 @@
 package me.henriquesouza.mcuml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import me.henriquesouza.mcuml.domain.Categoria;
+import me.henriquesouza.mcuml.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class McUmlApplication {
+public class McUmlApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McUmlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
