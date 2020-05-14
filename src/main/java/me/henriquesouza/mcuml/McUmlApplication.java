8c0f350977928562645ba4b636cf99d3f1de8559
Carlos Henrique <ch.ferreir@hotmail.com>
2020-05-13 23:43:54 -0300

    Estado e Cidade

--- src/main/java/me/henriquesouza/mcuml/McUmlApplication.java
+++ src/main/java/me/henriquesouza/mcuml/McUmlApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import me.henriquesouza.mcuml.domain.Categoria;
+import me.henriquesouza.mcuml.domain.Cidade;
+import me.henriquesouza.mcuml.domain.Estado;
 import me.henriquesouza.mcuml.domain.Produto;
 import me.henriquesouza.mcuml.repositories.CategoriaRepository;
+import me.henriquesouza.mcuml.repositories.CidadeRepository;
+import me.henriquesouza.mcuml.repositories.EstadoRepository;
 import me.henriquesouza.mcuml.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class McUmlApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McUmlApplication.class, args);
@@ -43,6 +51,19 @@ public class McUmlApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Paraná");
+		Estado est2 = new Estado(null, "Mato Grosso");
+		
+		Cidade c1 = new Cidade(null, "Umuarama", est1);
+		Cidade c2 = new Cidade(null, "Cuiabá", est2);
+		Cidade c3 = new Cidade(null, "Pontes e Lacerda", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
