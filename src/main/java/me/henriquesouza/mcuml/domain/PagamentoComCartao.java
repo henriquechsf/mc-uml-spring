5f9a3034a4055b45ded093abd2026eb523d5cd87
Carlos Henrique <ch.ferreir@hotmail.com>
2020-05-15 22:59:40 -0300

    Pedido, Pagamento, PagamentoComBoleto, PagamentoComCartao

---
+++ src/main/java/me/henriquesouza/mcuml/domain/PagamentoComCartao.java
@@ -0,0 +1,29 @@
+package me.henriquesouza.mcuml.domain;
+
+import me.henriquesouza.mcuml.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento {
+	// subclasse não precisa implementar Serializable
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroDeParcelas;
+
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+
+}
